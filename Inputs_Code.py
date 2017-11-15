dc852883be5cd786d1aac42a7d2b307461f0e1bd
Jacob-Blankenship <32876056+Jacob-Blankenship@users.noreply.github.com>
2017-11-14 21:29:01 -0600

    Update Inputs_Code.py
    add the processraster fun call

--- Inputs_Code.py
+++ Inputs_Code.py
@@ -1,7 +1,7 @@
-import arcpy
+import arcpy, Frame
 
 arcpy.env.workspace = arcpy.GetParameterAsText(0)      #this is where the files are to go
-input= arcpy.GetParameterAsText(1)                     # this is where the feature class is put
+input1= arcpy.GetParameterAsText(1)                     # this is where the feature class is put
 output= arcpy.GetParameterAsText(2)                    # for right now this is where the text file is saved just for testing
 User_Field = arcpy.GetParameterAsText(3)               # this is the string into which the user put the Field name
 User_Class = arcpy.GetParameterAsText(4)               # this is the string into which the user put the Class name
@@ -27,14 +27,14 @@ else:
 
 
 
-Fields=arcpy.ListFields(input)
+Fields=arcpy.ListFields(input1)
 for i in Fields:
     Fields_List.append(i.name)                                   # putting all of the Field in the feature class into the Fields_List
 if User_Field in Fields_List:
     file_object.write("Field Verified\n")                        # makes sure user put in the right field that have the class in them
     for i in Fields_List:
         file_object.write(i +"\n")
-    with arcpy.da.SearchCursor(input,[User_Field]) as Classes:   # this goes through the field of User_Field to find all of the class and then put them in the CLass list
+    with arcpy.da.SearchCursor(input1,[User_Field]) as Classes:   # this goes through the field of User_Field to find all of the class and then put them in the CLass list
         for i in Classes:
             if i[0] not in Class_List:
                 Class_List.append(i[0])                          # running through all of the Class and putting them in the Class_List
@@ -49,5 +49,10 @@ if User_Class in Class_List:
     file_object.write(Ratio)
     file_object.write(Cell_Size)
 
+xy=Cell_Size.split(" ")
+arcpy.AddMessage(xy)
+X=xy[0]
+Y=xy[1]
 
-#Parameters = classifiedRaster(input,Cell_Size,Ratio,User_Class)
+Parameters = Frame.classifiedRaster(input1,X,Y,Ratio,User_Class)
+Parameters.processRaster(output)
